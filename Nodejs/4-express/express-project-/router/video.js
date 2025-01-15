8827a7ccc207ca6244261a77ca56d3e406e4b7db
William Lao <c1471373444@outlook.com>
2025-01-15 10:54:31 +0800

    删除Express项目子模块，添加nodemon和其他依赖的配置文件

---
+++ Nodejs/4-express/express-project-/router/video.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const router = express.Router()
+const videoController=require('./controller/videoController')
+
+router
+  .get('/list', (req, res, next) => {
+    console.log(req.url)
+    res.send('/video-list')
+  })
+  .get('/route', (req, res, next) => {
+    console.log(req.url)
+    res.send('/route')
+  })
+
+module.exports = router
